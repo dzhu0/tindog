730c2e61029a64ba841af1b13fdc2ae109e55840
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-03-13 16:44:25 -0500

    Added more data

--- data.js
+++ data.js
@@ -22,6 +22,30 @@ const dogData = [
         bio: "How you doin?",
         hasBeenSwiped: false,
         hasBeenLiked: false
+    },
+    {
+        name: "Rex 2",
+        avatar: "./images/dog-rex.jpg",
+        age: 25,
+        bio: "Art. Literature. Natural wine. Yoga.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Bella 2",
+        avatar: "./images/dog-bella.jpg",
+        age: 43,
+        bio: "Yup, that's my owner. U can meet him if you want",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Teddy 2",
+        avatar: "./images/dog-teddy.jpg",
+        age: 30,
+        bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
     }
 ]
 
