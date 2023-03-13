483d70ef55f2e7549a1da4edf6f910ceada15641
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-03-13 14:38:56 -0500

    Functionalities added

--- Dog.js
+++ Dog.js
@@ -1,6 +1,34 @@
 class Dog {
-    constructor() {
+    constructor(data) {
+        Object.assign(this, data)
+    }
 
+    getProfileHtml() {
+        const {name, avatar, age, bio} = this
+
+        return `
+            <img
+                class="avatar"
+                src="${avatar}"
+                alt="User's avatar picture">
+            <div class="info">
+                <h1 class="name-age">${name}, ${age}</h1>
+                <p class="bio">${bio}</p>
+            </div>`
+    }
+
+    reject() {
+        this.hasBeenSwiped = true
+        this.hasBeenLiked = false
+    }
+
+    accept() {
+        this.hasBeenSwiped = true
+        this.hasBeenLiked = true
+    }
+
+    getObject() {
+        return this
     }
 }
 
--- index.js
+++ index.js
@@ -1,2 +1,77 @@
 import dogData from './data.js'
 import Dog from './Dog.js'
+
+const data = JSON.parse(localStorage.getItem('dogData')) || dogData
+
+const profile = document.getElementById('profile')
+const rejectBtn = document.getElementById('reject-btn')
+const acceptBtn = document.getElementById('accept-btn')
+
+let index = 0
+let dog = getNewDog()
+
+rejectBtn.addEventListener('click', reject)
+acceptBtn.addEventListener('click', accept)
+
+render()
+
+function render() {
+    profile.innerHTML = dog.getProfileHtml()
+
+    if (dog.hasBeenLiked) {
+        rejectBtn.classList.remove('rejected')
+        acceptBtn.classList.add('accepted')
+        profile.innerHTML += getBadgeHtml()
+    } else if (dog.hasBeenSwiped) {
+        rejectBtn.classList.add('rejected')
+        acceptBtn.classList.remove('accepted')
+        profile.innerHTML += getBadgeHtml()
+    }
+}
+
+function reject() {
+    dog.reject()
+    handleBtnClick()
+}
+
+function accept() {
+    dog.accept()
+    handleBtnClick()
+}
+
+function handleBtnClick() {
+    render()
+    
+    rejectBtn.classList.add('disabled')
+    acceptBtn.classList.add('disabled')
+
+    setTimeout(() => {
+        rejectBtn.classList.remove('disabled')
+        acceptBtn.classList.remove('disabled')
+        dog = getNewDog()
+        render()
+    }, 1000);
+
+    data[index++] = dog.getObject()
+    localStorage.setItem('dogData', JSON.stringify(data))
+}
+
+function getBadgeHtml() {
+    const badge = dog.hasBeenLiked ?
+        "./images/badge-like.png" :
+        "./images/badge-nope.png"
+
+    return `
+        <img
+            class="badge"
+            src="${badge}"
+            alt="A badge of accept or reject">`
+}
+
+function getNewDog() {
+    if (index === data.length) {
+        index = 0
+    }
+
+    return new Dog(data[index])
+}
